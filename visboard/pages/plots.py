5a6966b14d18ef18904a880db8612c9cace1e12b
Riley Thai <rileythai@proton.me>
2024-02-16 09:52:42 +1100

    fix: function colocation
    move plot settings to plots file, should help with the co-location

--- visboard/pages/plots.py
+++ visboard/pages/plots.py
@@ -9,12 +9,14 @@ import plotly.express as px
 import plotly.graph_objects as go
 import reacton.ipyvuetify as rv
 import solara as sl
-from plotly.graph_objs._figurewidget import FigureWidget
+from solara.components.card import Card
+from solara.components.columns import Columns
 from solara.lab import Menu, ContextMenu
 
 from state import State
-from plot_settings import show_settings
 from util import check_catagorical
+
+# TEMPLATES AND STATE
 
 DARK_TEMPLATE = dict(layout=go.Layout(
     font=dict(color="white", size=16),
@@ -136,6 +138,9 @@ def update_relayout(fig, relayout, plotstate):
         if "geo.projection.scale" in relayout.keys():
             fig.update_geos(projection_scale=relayout["geo.projection.scale"])
     return fig
+
+
+# SHOW PLOT
 
 
 def show_plot(type, del_func):
@@ -1036,3 +1041,180 @@ def skyplot(plotstate):
     #            ],
     #        )
     return
+
+
+# PLOT SETTINGS
+
+
+def show_settings(type, state):
+    # plot controls
+    if type == "scatter":
+        return scatter_menu(state)
+    elif type == "histogram":
+        return statistics_menu(state)
+    elif type == "histogram2d":
+        return aggregate_menu(state)
+    elif type == "skyplot":
+        return sky_menu(state)
+
+
+@sl.component()
+def sky_menu(plotstate):
+    df = State.df.value
+    columns = list(map(str, df.columns))
+    with sl.Columns([1, 1]):
+        with Card(margin=0):
+            with sl.Column():
+                sl.ToggleButtonsSingle(value=plotstate.geo_coords,
+                                       values=["ra/dec", "galactic lon/lat"])
+                sl.Select(
+                    label="Projection",
+                    value=plotstate.projection,
+                    values=plotstate.Lookup["projections"],
+                )
+            with sl.Row():
+                sl.Select(
+                    label="Color",
+                    values=columns,
+                    value=plotstate.color,
+                )
+                sl.Select(
+                    label="Colorscale",
+                    values=plotstate.Lookup["colorscales"],
+                    value=plotstate.colorscale,
+                )
+        with Card(margin=0):
+            with Columns([1, 1]):
+                with sl.Column():
+                    sl.Switch(label="Flip x", value=plotstate.flipx)
+                with sl.Column():
+                    sl.Switch(label="Flip y", value=plotstate.flipy)
+
+
+@sl.component()
+def scatter_menu(plotstate):
+    df = State.df.value
+    columns = list(map(str, df.columns))
+    with sl.Columns([1, 1]):
+        with Card(margin=0):
+            with Columns([1, 1]):
+                sl.Select(
+                    "Column x",
+                    values=columns,
+                    value=plotstate.x,
+                )
+                sl.Select(
+                    "Column y",
+                    values=columns,
+                    value=plotstate.y,
+                )
+            sl.Select(
+                label="Color",
+                values=columns,
+                value=plotstate.color,
+            )
+            sl.Select(
+                label="Colorscale",
+                values=plotstate.Lookup["colorscales"],
+                value=plotstate.colorscale,
+            )
+        with Card(margin=0):
+            with Columns([1, 1]):
+                with sl.Column():
+                    sl.Switch(label="Flip x", value=plotstate.flipx)
+                    sl.Switch(label="Log x", value=plotstate.logx)
+                with sl.Column():
+                    sl.Switch(label="Flip y", value=plotstate.flipy)
+                    sl.Switch(label="Log y", value=plotstate.logy)
+            sl.Markdown("### Reactive plotting")
+            sl.ToggleButtonsSingle(
+                values=["on", "off"],
+                value=plotstate.reactive,
+            )
+
+
+@sl.component()
+def statistics_menu(plotstate):
+    df = State.df.value
+    columns = list(map(str, df.columns))
+    with sl.Columns([1, 1]):
+        with Card(margin=0):
+            with sl.Column():
+                sl.Select(
+                    "Column x",
+                    values=columns,
+                    value=plotstate.x,
+                )
+        with Card(margin=0):
+            sl.SliderInt(
+                label="Number of Bins",
+                value=plotstate.nbins,
+                step=10,
+                min=10,
+                max=1e3,
+            )
+            sl.Select(
+                label="Normalization",
+                values=plotstate.Lookup["norms"],
+                value=plotstate.norm,
+            )
+    with Card(margin=0):
+        with sl.Columns([1, 1, 1], style={"align-items": "center"}):
+            sl.Switch(label="Log x", value=plotstate.logx)
+            sl.Switch(label="Flip x", value=plotstate.flipx)
+            sl.Switch(label="Log y", value=plotstate.logy)
+
+
+@sl.component()
+def aggregate_menu(plotstate):
+    df = State.df.value
+    columns = list(map(str, df.columns))
+    with sl.Columns([1, 1]):
+        with Card(margin=0):
+            with Columns([1, 1]):
+                with sl.Column():
+                    sl.Select(
+                        "Column x",
+                        values=columns,
+                        value=plotstate.x,
+                    )
+                with sl.Column():
+                    sl.Select(
+                        "Column y",
+                        values=columns,
+                        value=plotstate.y,
+                    )
+            sl.Select(
+                label="Colorscale",
+                values=plotstate.Lookup["colorscales"],
+                value=plotstate.colorscale,
+            )
+            with Columns([1, 1]):
+                with sl.Column():
+                    sl.Switch(label="Flip y", value=plotstate.flipy)
+                with sl.Column():
+                    sl.Switch(label="Flip x", value=plotstate.flipx)
+        with Card(margin=0):
+            sl.SliderInt(
+                label="Number of Bins",
+                value=plotstate.nbins,
+                step=2,
+                min=2,
+                max=500,
+            )
+            sl.Select(
+                label="Binning type",
+                values=plotstate.Lookup["bintypes"],
+                value=plotstate.bintype,
+            )
+            if str(plotstate.bintype.value) != "count":
+                sl.Select(
+                    label="Column to Bin",
+                    values=columns,
+                    value=plotstate.color,
+                )
+            sl.Select(
+                label="Binning scale",
+                values=plotstate.Lookup["binscales"],
+                value=plotstate.binscale,
+            )
