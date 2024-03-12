750b5cc4a8d420f0c45c664f1810ad20221f5ee9
Riley Thai <rileythai@proton.me>
2024-03-12 12:45:51 +1100

    add: 2nd plot in bug test file
    showing it also happens on scatterplots

--- slbug2.py
+++ slbug2.py
@@ -3,25 +3,23 @@ import plotly.express as px
 import solara as sl
 import vaex
 from plotly.graph_objs._figurewidget import FigureWidget
-from solara.components.columns import Columns
 
-df_sample = vaex.example()
+df_sample = vaex.example()  # happens on any df
 
 
 class PlotState:
-    """
-    Combination of reactive states which instantiate a specific plot's settings/properties
-    """
+    """Holds flip info"""
 
-    def __init__(self, type):
+    def __init__(self):
         self.flipx = sl.reactive(False)
         self.flipy = sl.reactive(False)
 
 
 @sl.component()
-def aggregate_menu(plotstate):
+def menu(plotstate):
+    """Switches for flipping"""
     with sl.Card():
-        with Columns([1, 1]):
+        with sl.Columns([1, 1]):
             with sl.Column():
                 sl.Switch(label="Flip y", value=plotstate.flipy)
             with sl.Column():
@@ -29,13 +27,57 @@ def aggregate_menu(plotstate):
 
 
 @sl.component()
-def aggregated(plotstate):
+def scatter(plotstate):
+    """Generates scatterplot"""
     df = df_sample
-    filter, set_filter = sl.use_cross_filter(id(df), "filter-aggregated")
 
     dff = df
-    if filter:
-        dff = df[filter]
+
+    def create_fig():
+        x = dff.x.values
+        y = dff.y.values
+        fig = px.scatter(x=x, y=y)
+        return fig
+
+    # only instantiate the figure once
+    figure = sl.use_memo(create_fig, dependencies=[])
+
+    def add_effects(fig_element: sl.Element):
+
+        def set_xflip():
+            fig_widget: FigureWidget = sl.get_widget(fig_element)
+
+            # flip
+            if plotstate.flipx.value:
+                fig_widget.update_xaxes(autorange="reversed")
+            else:
+                fig_widget.update_xaxes(autorange=True)
+            print(fig_widget.layout.xaxis)
+
+        def set_yflip():
+            print("setting yflip", plotstate.flipy.value)
+            fig_widget: FigureWidget = sl.get_widget(fig_element)
+            if plotstate.flipy.value:
+                fig_widget.update_yaxes(autorange="reversed")
+            else:
+                fig_widget.update_yaxes(autorange=True)
+            print(fig_widget.layout.yaxis)
+
+        sl.use_effect(set_xflip, dependencies=[plotstate.flipx.value])
+        sl.use_effect(set_yflip, dependencies=[plotstate.flipy.value])
+
+    fig_el = sl.FigurePlotly(figure)
+    add_effects(fig_el)
+
+    return
+
+
+@sl.component()
+def imshow(plotstate):
+    """Generates imshow plot with log10 scaling"""
+    df = df_sample
+
+    dff = df
 
     def perform_binning():
         expr = (dff.x, dff.y)
@@ -48,6 +90,7 @@ def aggregated(plotstate):
             array_type="xarray",
         )
 
+        # plotly doesn't support nan's -- have to change to -999
         y = np.log10(y)
         y = y.where(np.abs(y) != np.inf, np.nan)
         cmin = float(np.min(y).values)
@@ -62,8 +105,6 @@ def aggregated(plotstate):
                         zmax=cmax,
                         origin="lower",
                         color_continuous_scale="inferno")
-        fig.update_xaxes(autorange=False)
-        fig.update_yaxes(autorange=False)
         return fig
 
     # only instantiate the figure once
@@ -72,8 +113,9 @@ def aggregated(plotstate):
     def add_effects(fig_element: sl.Element):
 
         def set_xflip():
-            print("setting xflip", plotstate.flipx.value)
             fig_widget: FigureWidget = sl.get_widget(fig_element)
+
+            # flip
             if plotstate.flipx.value:
                 fig_widget.update_xaxes(autorange="reversed")
             else:
@@ -100,9 +142,11 @@ def aggregated(plotstate):
 
 @sl.component()
 def Page():
-    plotstate = PlotState("aggregated")
-    aggregated(plotstate)
-    aggregate_menu(plotstate)
+    plotstate = PlotState()
+    with sl.Columns([1, 1]):
+        imshow(plotstate)
+        scatter(plotstate)
+    menu(plotstate)
 
 
 if __name__ == "__main__":
