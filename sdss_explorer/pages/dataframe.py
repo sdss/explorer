bc2a157803d9335f74e44a5b5a71fabd567c9c05
Riley Thai <rileythai@proton.me>
2024-05-09 13:51:10 +1000

    feat: statistics view (#42)
    still very janky, need to write some vuetify code to make it look a lot better

--- sdss_explorer/pages/dataframe.py
+++ sdss_explorer/pages/dataframe.py
@@ -1,10 +1,22 @@
-from typing import cast, Optional
+import math
+from dataclasses import replace
+from typing import Callable, List, Optional, cast
+
+import solara
+import solara.hooks.dataframe
+import solara.lab
+from solara.lab.hooks.dataframe import use_df_column_names
+from solara.components.datatable import CellAction, ColumnAction, DataTableWidget
 
 import solara as sl
 from solara.lab import Menu
 import reacton.ipyvuetify as rv
+from solara.lab import use_task, Task
 
-from .state import State
+import pandas as pd
+
+from .subsets import use_subset
+from .state import State, VCData
 
 
 @sl.component
@@ -80,6 +92,161 @@ def show_table(del_func):
 
     else:
         NoDF()
+
+
+def format_default(df, column, row_index, value):
+    if isinstance(value, float) and math.isnan(value):
+        return "NaN"
+    return str(value)
+
+
+@sl.component
+def ModdedDataTable(
+    df,
+    page=0,
+    items_per_page=20,
+    format=None,
+    column_actions: List[ColumnAction] = [],
+    cell_actions: List[CellAction] = [],
+    scrollable=False,
+    on_column_header_hover: Optional[Callable[[Optional[str]], None]] = None,
+    column_header_info: Optional[sl.Element] = None,
+):
+    total_length = len(df)
+    options = {}
+    options, set_options = sl.use_state(options, key="options")
+    format = format or format_default
+    # frontend does 1 base, we use 0 based
+    page = 0
+    items_per_page = 7
+    i1 = page * items_per_page
+    i2 = min(total_length, (page + 1) * items_per_page)
+
+    columns = use_df_column_names(df)
+
+    items = []
+    dfs = df.iloc[i1:i2]
+    records = df.to_dict("records")
+    for i in range(i2 - i1):
+        item = {"__row__": dfs.index[i]}  # special key for the row number
+        for column in columns:
+            item[column] = format(dfs, column, i + i1, records[i][column])
+        items.append(item)
+
+    headers = [{
+        "text": name,
+        "value": name,
+        "sortable": False
+    } for name in columns]
+    column_actions_callbacks = [k.on_click for k in column_actions]
+    cell_actions_callbacks = [k.on_click for k in cell_actions]
+    column_actions = [replace(k, on_click=None) for k in column_actions]
+    cell_actions = [replace(k, on_click=None) for k in cell_actions]
+
+    return DataTableWidget.element(
+        total_length=total_length,
+        items=items,
+        headers=headers,
+        headers_selections=[],
+        options=options,
+        items_per_page=items_per_page,
+        selections=[],
+        selection_colors=[],
+        selection_enabled=False,
+        highlighted=None,
+        scrollable=scrollable,
+        on_options=set_options,
+        column_actions=column_actions,
+        cell_actions=cell_actions,
+        _column_actions_callbacks=column_actions_callbacks,
+        _cell_actions_callbacks=cell_actions_callbacks,
+        on_column_header_hover=on_column_header_hover,
+        column_header_widget=column_header_info,
+    )
+
+
+@sl.component
+def DescribeDF(del_func):
+    """Statistics description view for the dataset."""
+    df = State.df.value
+    subset = sl.use_reactive(State.subsets.value[0])  # inits with first subset
+    filter, set_filter = use_subset(subset, name="statsview")
+    columns, set_columns = sl.use_state(["teff", "logg", "fe_h"])
+
+    if filter:
+        dff = df[filter]
+    else:
+        dff = df
+
+    # the summary table is its own DF (for render purposes)
+    # NOTE: worker process concerns if this takes more than 10MB.
+
+    def generate_describe() -> pd.DataFrame:
+        """Generates the description table only on column/filter updates"""
+        # INFO: vaex returns a pandas df.describe()
+        return dff[columns].describe(strings=False)
+
+    result: Task[pd.DataFrame] = use_task(
+        generate_describe, dependencies=[filter, columns,
+                                         len(columns)])
+
+    def remove_column(name):
+        """Removes column from column list"""
+        # perform removal via slice (cannot modify inplace)
+        # TODO: check if slicing is actually necessary
+
+        q = None
+        for i, col in enumerate(columns):
+            if col == name:
+                q = i
+                break
+
+        set_columns(columns[:q] + columns[q + 1:])
+
+    column_actions = [
+        # TODO: a more complex action in here?
+        sl.ColumnAction(icon="mdi-delete",
+                        name="Remove column",
+                        on_click=remove_column),
+    ]
+    print(result.finished)
+    print(result.value)
+
+    with rv.Card(class_="grey darken-3",
+                 style_="width: 100%; height: 100%") as main:
+        with rv.CardText():
+            with sl.Column(classes=["grey darken-3"]):
+                sl.ProgressLinear(result.pending)
+                if ~result.not_called and result.latest is not None:
+                    ModdedDataTable(
+                        result.latest,
+                        items_per_page=7,
+                        column_actions=column_actions,
+                    )
+                else:
+                    NoDF()
+                btn = sl.Button(
+                    icon_name="mdi-settings",
+                    outlined=False,
+                    classes=["grey darken-3"],
+                )
+                # settings menu
+                with Menu(activator=btn, close_on_content_click=False):
+                    with sl.Card(margin=0):
+                        with sl.Columns([1]):
+                            sl.SelectMultiple(
+                                label="Columns",
+                                values=columns,
+                                all_values=State.columns.value,
+                                on_value=set_columns,
+                            )
+                        sl.Button(
+                            icon_name="mdi-delete",
+                            color="red",
+                            block=True,
+                            on_click=del_func,
+                        )
+    return main
 
 
 @sl.component
--- sdss_explorer/pages/grid.py
+++ sdss_explorer/pages/grid.py
@@ -9,7 +9,7 @@ import ipyvuetify as v
 import ipywidgets as widgets
 
 from .plots import show_plot
-from .dataframe import show_table
+from .dataframe import show_table, DescribeDF
 from .state import State
 
 
@@ -67,10 +67,10 @@ def ViewCard(type, i):
         # INFO: cannot be deleted because it breaks all renders
         GridState.objects.value[i] = rv.Card()
 
-    if type != "table":
+    if type != "stats":
         show_plot(type, lambda: remove(i))
     else:
-        show_table(lambda: remove(i))
+        DescribeDF(lambda: remove(i))
 
     return
 
@@ -92,8 +92,8 @@ def ObjectGrid():
         else:
             prev = GridState.grid_layout.value[-1]
         # set height based on type
-        if type == "table":
-            height = 10
+        if type == "stats":
+            height = 8
         else:
             height = 12
         i = GridState.index
@@ -109,22 +109,6 @@ def ObjectGrid():
 
         GridState.objects.value = GridState.objects.value + [ViewCard(type, i)]
 
-    def add_histogram():
-        add_view("histogram")
-
-    def add_aggregated():
-        add_view("aggregated")
-
-    def add_scatter():
-        add_view("scatter")
-
-    def add_skyplot():
-        add_view("skyplot")
-
-    def add_table():
-        add_view("table")
-
-    length = len(State.subsets.value)
     with sl.Column(style={"width": "100%"}) as main:
         with sl.Row():
             btn = sl.Button(
@@ -132,38 +116,33 @@ def ObjectGrid():
                 outlined=False,
                 icon_name="mdi-image-plus",
             )
-            deltabtn = sl.Button(
-                "Add Comparative View",
-                disabled=True if length <= 1 else False,
-                outlined=False,
-                icon_name="mdi-image-multiple",
-            )
             with Menu(activator=btn):
                 with sl.Column(gap="0px"):
                     [
                         sl.Button(label="histogram",
                                   on_click=lambda: add_view("histogram")),
-                        sl.Button(label="heatmap", on_click=add_aggregated),
-                        sl.Button(label="table", on_click=add_table),
-                        sl.Button(label="scatter", on_click=add_scatter),
-                        sl.Button(label="skyplot", on_click=add_skyplot),
-                    ]
-            with Menu(activator=deltabtn):
-                with sl.Column(gap="0px"):
-                    # TODO: change names to be more informative
-                    [
-                        sl.Button(label="delta1d",
-                                  on_click=lambda: add_view("delta1d")),
-                        sl.Button(label="delta2d",
-                                  on_click=lambda: add_view("delta2d")),
+                        sl.Button(label="heatmap",
+                                  on_click=lambda: add_view("heatmap")),
+                        sl.Button(label="stats",
+                                  on_click=lambda: add_view("stats")),
+                        sl.Button(label="scatter",
+                                  on_click=lambda: add_view("scatter")),
+                        sl.Button(label="skyplot",
+                                  on_click=lambda: add_view("skyplot")),
+                        sl.Button(
+                            label="delta2d",
+                            on_click=lambda: add_view("delta2d"),
+                            disabled=True
+                            if len(State.subsets.value) <= 1 else False,
+                        ),
                     ]
             rv.Spacer()
-            sl.Button(
-                color="yellow",
-                icon_name="mdi-refresh",
-                classes=["black--text"],
-                on_click=reset_layout,
-            )
+            # sl.Button(
+            #    color="yellow",
+            #    icon_name="mdi-refresh",
+            #    classes=["black--text"],
+            #    on_click=reset_layout,
+            # )
         GridDraggableToolbar(
             items=GridState.objects.value,
             grid_layout=GridState.grid_layout.value,
