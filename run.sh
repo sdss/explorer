1fac26080dc69d15f7e4d4fc5715598c478157eb
Riley Thai <rileythai@proton.me>
2024-10-26 14:16:36 +1100

    feat: remove solara banner
    - it actually gets in the way if you have a lot of plots open

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-EXPLORER_DEV=True VAEX_CACHE="memory,disk" VAEX_CACHE_DISK_SIZE_LIMIT="10GB" VAEX_CACHE_MEMORY_SIZE_LIMIT="1GB" solara run --theme-variant=dark --log-level=debug --log-level-uvicorn=info sdss_explorer.pages
+SOLARA_THEME_SHOW_BANNER=False EXPLORER_DEV=True VAEX_CACHE="memory,disk" VAEX_CACHE_DISK_SIZE_LIMIT="10GB" VAEX_CACHE_MEMORY_SIZE_LIMIT="1GB" solara run --theme-variant=dark --log-level=debug --log-level-uvicorn=info sdss_explorer.pages
