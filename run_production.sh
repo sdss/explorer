7066629b30cf377ab3e6db76c075d626e5f3dc9f
Riley Thai <rileythai@proton.me>
2025-01-14 11:59:25 +1100

    fix: local runners for solara 1.42.0
    - be quiet about hook checks

--- run_production.sh
+++ run_production.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-SOLARA_KERNEL_CULL_TIMEOUT=5s SOLARA_THEME_SHOW_BANNER=False VAEX_CACHE="memory,disk" VAEX_CACHE_DISK_SIZE_LIMIT="10GB" VAEX_CACHE_MEMORY_SIZE_LIMIT="1GB" solara run --workers=1 --production --theme-variant=dark --host=0.0.0.0 sdss_explorer.pages
-
+SOLARA_CHECK_HOOKS=off SOLARA_KERNEL_CULL_TIMEOUT=5m SOLARA_THEME_SHOW_BANNER=False VAEX_CACHE="memory,disk" VAEX_CACHE_DISK_SIZE_LIMIT="10GB" VAEX_CACHE_MEMORY_SIZE_LIMIT="1GB" solara run --workers=1 --production --theme-variant=dark --host=0.0.0.0 sdss_explorer.pages
