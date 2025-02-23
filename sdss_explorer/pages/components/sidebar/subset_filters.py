d335b417a874c8c9a497ffef8df51b50795cc76a
Riley Thai <rileythai@proton.me>
2025-02-23 18:58:45 +1100

    feat: publicized filter methods for download api
    - may want to move the module namespace at some point, but its good atm

--- sdss_explorer/pages/components/sidebar/subset_filters.py
+++ sdss_explorer/pages/components/sidebar/subset_filters.py
@@ -13,6 +13,13 @@ import reacton.ipyvuetify as rv
 import solara as sl
 from reacton.core import ValueElement
 
+from ....filter_functions import (
+    filter_carton_mapper,
+    filter_expression,
+    filter_flags,
+    flagList,
+)
+
 from ...dataclass import Alert, State, SubsetState, use_subset, VCData
 from ..textfield import InputTextExposed
 from .autocomplete import AutocompleteSelect, SingleAutocomplete
@@ -23,18 +30,6 @@ __all__ = [
 ]
 
 logger = logging.getLogger("sdss_explorer")
-
-operator_map = {"AND": operator.and_, "OR": operator.or_, "XOR": operator.xor}
-
-flagList = {
-    # TODO: more quick flags based on scientist input
-    "sdss5 only": "release=='sdss5'",
-    "snr > 50": "snr>=50",
-    "purely non-flagged": "result_flags==0",
-    #'no apo 1m': "telescope!='apo1m'", # WARNING: this one doesn't work for some reason, maybe it's not string; haven't checked
-    "no bad flags": "flag_bad==0",
-    "gmag < 17": "g_mag<=17",
-}
 
 
 @sl.component()
@@ -60,94 +55,19 @@ def ExprEditor(key: str, invert) -> ValueElement:
         a precise error message for any issues in the expression.
         """
         columns = State.df.value.get_column_names()
+        if expression is None or expression == "" or expression == "None":
+            set_expfilter(None)
+            set_expression("")
+            return None
         try:
-            if expression is None or expression == "" or expression == "None":
-                set_expfilter(None)
-                set_expression("")
-                return None
-            # first, remove all spaces
-            expr = expression.replace(" ", "")
-            num_regex = r"^-?[0-9]+(?:\.[0-9]+)?(?:e-?\d+)?$"
-
-            # get expression in parts, saving split via () regex
-            subexpressions = re.split(r"(&|\||\)|\()", expr)
-            n = 1
-            for i, expr in enumerate(subexpressions):
-                # saved regex info -> skip w/o enumerating
-                if expr in ["", "&", "(", ")", "|"]:
-                    continue
-
-                parts = re.split(r"(>=|<=|<|>|==|!=)", expr)
-                if len(parts) == 1:
-                    assert False, f"expression {n} is invalid: no comparator"
-                elif len(parts) == 5:
-                    # first, check that parts 2 & 4 are lt or lte comparators
-                    assert (
-                        re.fullmatch(r"<=|<", parts[1]) is not None
-                        and re.fullmatch(r"<=|<", parts[3]) is not None
-                    ), f"expression {n} is invalid: not a proper 3-part inequality (a < col <= b)"
-
-                    # check middle
-                    assert (
-                        parts[2] in columns
-                    ), f"expression {n} is invalid: must be comparing a data column (a < col <= b)"
-
-                    # check a and b & if a < b
-                    assert (
-                        re.match(num_regex, parts[0]) is not None
-                    ), f"expression {n} is invalid: must be numeric for numerical data column"
-                    assert (
-                        float(parts[0]) < float(parts[-1])
-                    ), f"expression {n} is invalid: invalid inequality (a > b for a < col < b)"
-
-                    # change the expression to valid format
-                    subexpressions[i] = (
-                        f"(({parts[0]}{parts[1]}{parts[2]})&({parts[2]}{parts[3]}{parts[4]}))"
-                    )
-
-                elif len(parts) == 3:
-                    check = (parts[0] in columns, parts[2] in columns)
-                    if np.any(check):
-                        if check[0]:
-                            col = parts[0]
-                            num = parts[2]
-                        elif check[1]:
-                            col = parts[2]
-                            num = parts[0]
-                        dtype = str(df[col].dtype)
-                        if "float" in dtype or "int" in dtype:
-                            assert (
-                                re.match(num_regex, num) is not None
-                            ), f"expression {n} is invalid: must be numeric for numerical data column"
-                    else:
-                        assert (
-                            False
-                        ), f"expression {n} is invalid: one part must be column"
-                    assert (
-                        re.match(r">=|<=|<|>|==|!=", parts[1]) is not None
-                    ), f"expression {n} is invalid: middle is not comparator"
-
-                    # change the expression in subexpression
-                    subexpressions[i] = "(" + expr + ")"
-                else:
-                    assert False, f"expression {n} is invalid: too many comparators"
-
-                # enumerate the expr counter
-                n = n + 1
-
-            # create expression as str
-            expr = "(" + "".join(subexpressions) + ")"
-
-            # set filter corresponding to inverts & exit
-            if invert.value:
-                set_expfilter(~df[expr])
-            else:
-                set_expfilter(df[expr])
+            exprfilter = filter_expression(df,
+                                           columns,
+                                           expression,
+                                           invert=invert.value)
+            set_expfilter(exprfilter)
             return True
-
         except AssertionError as e:
-            # INFO: it's probably better NOT to unset filters if assertions fail.
-            # set_filter(None)
+            # INFO: dont unset filters if assertions fail; tell user and let them try again
             set_error(e)  # saves error msg to state
             return False
         except SyntaxError:
@@ -284,26 +204,13 @@ def FlagSelect(key: str, invert) -> ValueElement:
     )
 
     def update_flags():
-        filters = []
         # TODO: relocate this somewhere better, like into a lookup dataclass or a file
         # get the flag lookup
         if flags:
-            for flag in flags:
-                # Skip iteration if the subset's dataset is 'best' and the flag is 'Purely non-flagged'
-                if (subset.dataset == "best") and (flag
-                                                   == "purely non-flagged"):
-                    continue
-                filters.append(flagList[flag])
-
-            # Determine the final concatenated filter
-            if filters:
-                # Join the filters with ")&(" and wrap them in outer parentheses
-                concat_filter = f"(({')&('.join(filters)}))"
-                concat_filter = df[concat_filter]
-                if invert.value:
-                    concat_filter = ~concat_filter
-            else:
-                concat_filter = None
+            concat_filter = filter_flags(df,
+                                         flags,
+                                         subset.dataset,
+                                         invert=invert.value)
         else:
             concat_filter = None
 
@@ -360,44 +267,20 @@ def TargetingFiltersPanel(key: str, invert) -> ValueElement:
         logger.info(f"MAPPER: {mapper}")
         logger.info(f"CARTON: {carton}")
         start = timer()
-        if len(mapper) != 0 or len(carton) != 0:
-            # mask
-            if len(mapper) == 0:
-                mask = mapping["alt_name"].isin(carton).values
-            elif len(carton) == 0:
-                mask = mapping["mapper"].isin(mapper).values
-            else:
-                mask = operator_map[combotype](
-                    mapping["mapper"].isin(mapper).values,
-                    mapping["alt_name"].isin(carton).values,
-                )
-
-            # determine active bits via mask and get flag_number & offset
-            # NOTE: hardcoded nbits as 8, and nflags as 57
-            bits = np.arange(len(mapping))[mask]
-            num, offset = np.divmod(bits, 8)
-            setbits = 57 > num  # ensure bits in flags
-
-            # construct hashmap for each unique flag
-            filters = np.zeros(57, dtype="uint8")
-            unique_nums, indices = np.unique(num[setbits], return_inverse=True)
-            for i, unique in enumerate(unique_nums):
-                offsets = 1 << offset[setbits][indices == i]
-                filters[unique] = np.bitwise_or.reduce(offsets)
-
-            cmp_filter = df.func.check_flags(df["sdss5_target_flags"], filters)
-        else:
-            cmp_filter = None
-
+        cmp_filter = filter_carton_mapper(
+            df,
+            mapping,
+            carton,
+            mapper,
+            combotype=combotype,
+            invert=invert.value,
+        )
         # invert if requested
-        if invert.value:
-            set_cmfilter(~cmp_filter)
-        else:
-            set_cmfilter(cmp_filter)
+        set_cmfilter(cmp_filter)
 
         logger.info(f"CM filter took {timer() - start:.4f} seconds")
 
-        return cmp_filter
+        return
 
     result = sl.lab.use_task(update_cm,
                              dependencies=[df, mapper, carton, invert.value])
